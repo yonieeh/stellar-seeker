5d55eb8b23378d0712da2a747376d9fd0c586552
Martin Vidal <martin.vidal@uc.cl>
2024-12-25 05:10:50 -0300

    finished the feature of the bot being able to send two separate welcome messages to a server

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import { CommandClient } from "eris";
+import { readFileSync } from "fs";
 
 const bot = new CommandClient(process.env.DISCORD_TOKEN, { intents: ["guilds", "guildMessages", "messageContent", "guildMembers"]}, {
   description: "Stellar seeker bot, made for Stellar://Grove",
@@ -25,17 +26,55 @@ bot.registerCommand("pong", ["Pang!", "Peng!", "Ping!", "Pung!"], {
   fullDescription: "This command could also be used to check if the bot is up. Or entertainment when you're bored.",
 });
 
-const embed = {
-  title: "Welcome!",
-  description: "╰> Check out the (rules)[#1319901737972269076]\n╰> Assign yourself (roles)[#1319901782280900618]!",
-  color: 0x7289da 
-};
+
 
 bot.on("guildMemberAdd", (guild, member) => {
   console.log(`${member.username} has joined the server!`);
-  bot.createMessage(1319566683047727157n, { embed });
-  bot.createMessage(1319566715042005044n, `welcome 🦦 - <@${member.id}>\n `);
-  bot.createMessage(1319566715042005044n, { embed } )
+  
+  if (guild.id === "915709191879020664") { // test server
+    const embed = {
+      title: "welcome 🦦",
+      description: "✨ check out the rules in <#1319901737972269076>\n✨ assign yourself roles in <#1319901782280900618>\n✨ vote in decisions for the server in <#1321233121479491645>",
+      color: 0x7289da, 
+      thumbnail: {
+        url: member.avatarURL 
+      }
+    };
+
+    bot.createMessage(1319566715042005044n, {
+      content:`** **                          ‧͙⁺˚･༓☾stellar://grove☽༓･˚⁺‧͙ \n ** **                                       <@${member.id}>`,
+      embeds: [embed] 
+    });
+
+    bot.createMessage(1319566683047727157n, {
+      content:`welcome to stellar grove ${member.id}! make sure to read the faq and the rules, and enjoy your stay!` 
+    }, {
+      file: readFileSync("./assets/welcome-image.png"),
+      name: "welcome.png"
+    });
+  } else if (guild.id === "968340299132858420") { // main server
+    const embed = {
+      title: "welcome 🦦",
+      description: "✨ check out the rules in <#1311157085894873190>\n✨ assign yourself roles in <#1311885275902771212>\n✨ vote in decisions for the server in <#1311845120127012975>",
+      color: 0x7289da, 
+      thumbnail: {
+        url: member.avatarURL 
+      }
+    };
+
+    bot.createMessage(1311157095034388661n, {
+      content:`** **                          ‧͙⁺˚･༓☾stellar://grove☽༓･˚⁺‧͙ \n ** **                                       <@${member.id}>`,
+      embeds: [embed] 
+    });
+    
+    //bot.createMessage(1311157094044405871n, {
+    //  content:`welcome to stellar grove ${member.id}! make sure to read the faq and the rules, and enjoy your stay!` 
+    //}, {
+    //  file: readFileSync("./assets/welcome-image.png"),
+    //  name: "welcome.png"
+    //});
+  }
+    
 });
 
 bot.connect();
